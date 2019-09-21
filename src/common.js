be2b71db91c9cd7af2c43cde647cec6be9fe62a1
cicero.barreto <cicero.barreto@dataprev.gov.br>
2019-09-21 08:53:31 -0300

    Ajuste MSG e Alteração nome do projeto

--- src/common.js
+++ src/common.js
@@ -4,7 +4,7 @@ const server = Platform.OS === 'ios' ?
     'http://localhost:3001' : 'http://localhost:3001'
 
 function showError(err) {
-    Alert.alert('Ops! Ocorreu um erro', `Mensagem: ${err}`)
+    Alert.alert('Ops! Ocorreu um erro', `Mensagem: ${err.response.data.errors.errors[0]}`)
 }
 
 export { server, showError }
